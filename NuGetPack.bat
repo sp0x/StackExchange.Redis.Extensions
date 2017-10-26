313771832391726ce0d09723bc61e4395e9c22b1
vasko <sp0x@xakep.ru>
2017-10-26 17:56:45 +0300

    Updated package ids

--- NuGetPack.bat
+++ NuGetPack.bat
@@ -1,11 +1,3 @@
-.\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.Core\StackExchange.Redis.Extensions.Core.csproj -Prop Configuration=Release -OutputDirectory .\nuget\
+dotnet pack src\StackExchange.Redis.Extensions.Core\StackExchange.Redis.Extensions.Core.csproj -o .\..\..\nuget
 
-.\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.Jil\StackExchange.Redis.Extensions.Jil.csproj -Prop Configuration=Release -OutputDirectory .\nuget 
-
-.\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.Newtonsoft\StackExchange.Redis.Extensions.Newtonsoft.csproj -Prop Configuration=Release -OutputDirectory .\nuget
-
-.\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.MsgPack\StackExchange.Redis.Extensions.MsgPack.csproj -Prop Configuration=Release -OutputDirectory .\nuget
-
-.\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.Protobuf\StackExchange.Redis.Extensions.Protobuf.csproj -Prop Configuration=Release -OutputDirectory .\nuget
-
-.\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.Binary\StackExchange.Redis.Extensions.Binary.csproj -Prop Configuration=Release -OutputDirectory .\nuget
+dotnet pack src\StackExchange.Redis.Extensions.Jil\StackExchange.Redis.Extensions.Jil.csproj -o .\..\..\nuget 
